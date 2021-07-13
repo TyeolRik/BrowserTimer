122e5e1e63366fd2045cee6f0c531281ad661a8e
TyeolRik <kino6147@naver.com>
2021-07-13 14:53:47 +0900

    Complete

---
+++ BrowserTimer/BrowserTimer/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace BrowserTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.onOffButton = new System.Windows.Forms.Button();
+            this.TrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.TrayIcon_Context = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TrayIcon_Context.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // onOffButton
+            // 
+            this.onOffButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.onOffButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.onOffButton.Font = new System.Drawing.Font("Consolas", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.onOffButton.Location = new System.Drawing.Point(0, 0);
+            this.onOffButton.Name = "onOffButton";
+            this.onOffButton.Size = new System.Drawing.Size(392, 138);
+            this.onOffButton.TabIndex = 0;
+            this.onOffButton.Text = "OFF";
+            this.onOffButton.UseVisualStyleBackColor = false;
+            this.onOffButton.Click += new System.EventHandler(this.OnclickedONOFFButton);
+            // 
+            // TrayIcon
+            // 
+            this.TrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayIcon.Icon")));
+            this.TrayIcon.Text = "BrowserTimer";
+            this.TrayIcon.Visible = true;
+            // 
+            // TrayIcon_Context
+            // 
+            this.TrayIcon_Context.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.TrayIcon_Context.Name = "contextMenuStrip1";
+            this.TrayIcon_Context.Size = new System.Drawing.Size(111, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.showToolStripMenuItem.Text = "보이기";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Clicked);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.exitToolStripMenuItem.Text = "끝내기";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Clicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 138);
+            this.Controls.Add(this.onOffButton);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BrowserTimer";
+            this.TrayIcon_Context.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button onOffButton;
+        private System.Windows.Forms.NotifyIcon TrayIcon;
+        private System.Windows.Forms.ContextMenuStrip TrayIcon_Context;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+   
+    }
+}
+
---
+++ BrowserTimer/BrowserTimer/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowserTimer
+{
+    public partial class Form1 : Form
+    {
+        public static bool isWorking;      // Checking Button is ON or OFF
+        public static int TIME_INTERVAL = 10000;    // 밀리초 기준입니다. 10000밀리초 = 10초 ㅇㅋ?
+        public static string REDIRECT_URL = "https://www.naver.com";
+
+        public Form1()
+        {
+            InitializeComponent();
+            isWorking = false;
+
+            this.Load += TrayIcon_Load;
+            this.FormClosing += DicForm_FormClosing;
+
+            TrayIcon.MouseDoubleClick += TrayIcon_MouseDoubleClick;
+            showToolStripMenuItem.Click += showToolStripMenuItem_Clicked;
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Clicked;
+        }
+
+        private void DicForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visible = false;
+        }
+
+        public void TrayIcon_Load(object sender, EventArgs e)
+        {
+            TrayIcon.ContextMenuStrip = TrayIcon_Context;
+        }
+
+        public void TrayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void showToolStripMenuItem_Clicked(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void exitToolStripMenuItem_Clicked(object sender, EventArgs e)
+        {
+            MessageBox.Show("완전히 종료합니다.");
+            Environment.Exit(0);
+        }
+
+        private Timer backgroundTimer;
+
+        private void OnclickedONOFFButton(object sender, EventArgs e)
+        {
+            isWorking = !isWorking;
+            if (isWorking)
+            {
+                // ON
+                onOffButton.BackColor = Color.FromArgb(255, 192, 255, 192);
+                onOffButton.Text = "ON";
+
+                // Setting Timer
+                backgroundTimer = new Timer();
+                backgroundTimer.Tick += new EventHandler(backgroundWork);
+                backgroundTimer.Interval = TIME_INTERVAL;
+                backgroundTimer.Start();
+            }
+            else
+            {
+                // OFF
+                onOffButton.BackColor = Color.FromArgb(255, 255, 192, 192);
+                onOffButton.Text = "OFF";
+                backgroundTimer.Stop();
+            }
+        }
+
+
+        private void backgroundWork(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(REDIRECT_URL);
+        }
+    }
+}
